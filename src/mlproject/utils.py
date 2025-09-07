1e5752475c885f32d0fbcbe264a39f9315361ef3
Aabhas2 <sharma2.aabhas@gmail.com>
2025-09-08 02:28:29 +0530

    Initiated Data Ingestion

---
+++ src/mlproject/utils.py
@@ -0,0 +1,11 @@
+import os 
+import sys 
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd 
+from dotenv import load_dotenv
+
+
+
+def read_sql_data():
+    pass 
